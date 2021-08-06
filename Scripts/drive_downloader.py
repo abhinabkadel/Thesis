8680229496e2684590812165797fde6508db7f50
abhinabkadel <akadel@eoas.ubc.ca>
2021-08-06 20:20:37 +0545

    added script to download files from google drive

--- Scripts/drive_downloader.py
+++ Scripts/drive_downloader.py
@@ -1,5 +1,26 @@
 # %%
-import os 
+import os
+import pandas as pd
 
 # %%
-os.system("wsl rclone lsf --drive-shared-with-me WFRTGdrive_remote:test_AK/")
+src_rt      = r"wfrt:/reforecasts-2/"
+src_dir     = pd.date_range(start='20140701', end='20140715').strftime("%Y%m%d.%H").values
+dst_rt      = r"D:\Masters\Thesis\Test_downloads"
+# rclone_cmd  = "rclone copy --verbose --update --progress --dry-run "
+rclone_cmd  = "rclone copy --verbose --update --progress "
+# other options --interactive 
+
+
+# %%
+for folder in src_dir:
+    print(folder)
+    print("\n")
+    for i in [*range(1, 16), 52]:
+        print(i)
+        fname = f"Qout_south_asia_mainland_{i:d}.nc"
+        src_pth = src_rt + folder + "/" + fname
+        dst_pth = os.path.join(dst_rt, folder[:-3])
+        os.system(rclone_cmd + src_pth + " " + dst_pth)
+    
+
+# %%
