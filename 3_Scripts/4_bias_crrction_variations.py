67a0bb9156b3ac55623c9966b470d0786e4e1229
abhinabkadel <akadel@eoas.ubc.ca>
2022-11-06 19:00:15 -0800

    calibration and verification of bias correction parametners

--- 3_Scripts/4_bias_crrction_variations.py
+++ 3_Scripts/4_bias_crrction_variations.py
@@ -12,13 +12,15 @@ warnings.filterwarnings("ignore")
 """
 ####### Initialization parameters and get frcst data ######
 """
-def get_fcst_data (date_range):
-    # site name and associated comID:
-    site            = "Balephi"
+def get_fcst_data (date_range, site):
+
     ## set file path:
     rt_dir          = r"../1_Data/Fcst_data"
     obs_dir         = r"../1_Data/obs_data"
-    site_comID      = pd.read_pickle (r"./Sites_info/sites_tbl.pkl").loc[site].values[0]
+    site_comID      = pd.read_pickle (
+            r"./Sites_info/sites_tbl.pkl").loc[site].values[0]
+
+    print(site_comID)
     # date list of interest:
     ens_members     = [*range(1,53)]
 
@@ -33,9 +35,120 @@ def get_fcst_data (date_range):
         (slice(None), slice(None), slice(date_range[0], date_range[1]))
     ]
 
-    return site, obs_dir, fcst_data
+    return obs_dir, fcst_data
+
+def dmb_vars_test(fcst_types, days, win_len, site, fcst_data, obs_dir): 
+# tests the 4 different variations of degree of mass balance:
+
+    lo_verif    = []
+    hi_verif    = []
+    prob_verif  = []
+    # loop through the forecast horizons
+    for day in days:
+
+        # add observations:
+        [fcst_data_day, clim_vals] = add_obs(
+        place = site, fcst_df = fcst_data, obs_dir = obs_dir, day = day)
+
+        bc_df   = bc_fcsts_variations(fcst_data_day, win_len)   
+        df_det  = det_frcsts(bc_df.reset_index(),
+                fcst_types)
+
+        # loop through the two dataframes to create:
+        for flo_con in ["low", "high"]:
+
+            # defines dataframes for low_flow and high_flow values
+            if flo_con == "low":
+                df      = df_det[df_det["Obs"] <= clim_vals["q60_flo"]]
+                df_p    = bc_df[bc_df["Obs"] <= clim_vals["q60_flo"]]
+                flo_mean = clim_vals["lo_flo_clim"]
+
+            else:
+                df      = df_det[df_det["Obs"] > clim_vals["q60_flo"]]
+                df_p    = bc_df[bc_df["Obs"] > clim_vals["q60_flo"]]
+                flo_mean = clim_vals["hi_flo_clim"]
+
+            # empty list that holds the information on the verification:
+            data = []
+            
+            # loop through the raw and bias corrected forecasts:
+            for i in fcst_types:
+                # NSE:
+                nse = df.groupby(by = "det_frcst").apply(
+                        lambda x:nse_form(x, flo_mean, i)
+                    )
+                # NSE for individual ensemble members as det frcsts:
+                nse_all_mem = df_p.groupby(by = "ens_mem").apply(
+                        lambda x:nse_form(x, flo_mean, i)
+                    )            
+
+                # KGE:
+                kge = df.groupby(by = "det_frcst").apply(
+                        lambda x:kge_form(x, i)
+                    )
+                # KGE for individual ensemble members as det frcsts:
+                kge_all_mem = df_p.groupby(by = "ens_mem").apply(
+                        lambda x:kge_form(x, i)
+                    )
+
+                # concatenate and create a dataframe
+                verifs = pd.concat(
+                            [ pd.concat( [ nse, nse_all_mem ] ), 
+                                pd.concat( [ kge.droplevel(1), kge_all_mem.droplevel(1)] )]
+                            , axis = 1).set_axis(
+                                ["NSE", "r", "flo_var", "bias", "KGE"], axis = 1
+                        )
+                # new index with the fcst_type information:
+                verifs["fcst_type"] = i
+
+                data.append(verifs)
+
+            # end for along fcst_types
+
+            if flo_con == "low":
+                lo_df = pd.concat(data)
+                lo_df.index.rename("det_frcst", inplace = True)
+
+            else : 
+                hi_df = pd.concat(data)
+                hi_df.index.rename("det_frcst", inplace = True)
+
+        lo_df, hi_df
+
+        # calculate probabilitic verification (CRPS):
+        prob_df = prob_metrics(bc_df, clim_vals, fcst_types)
+
+        lo_df["day"]   = day
+        hi_df["day"]   = day
+        prob_df["day"] = day
+
+        # one large df with day information
+        lo_verif.append(lo_df) 
+        hi_verif.append(hi_df)
+        prob_verif.append(prob_df)
+
+    # create a single large dataframe for low and high flow seasons:
+    lo_verif    = pd.concat(lo_verif)
+    hi_verif    = pd.concat(hi_verif)
+    prob_verif  = pd.concat(prob_verif)
+
+    lo_verif["flow_clim"]   = "low"
+    hi_verif["flow_clim"]   = "high"
+
+    det_verif   = pd.concat([lo_verif, hi_verif])
+
+    det_verif    = det_verif.set_index(["day", "flow_clim", "fcst_type"], append= True
+                    ).reorder_levels(["day", "flow_clim", "fcst_type", "det_frcst"]).sort_index()
+    prob_verif  = prob_verif.set_index(["day"], append= True
+                    ).reorder_levels(["day", "flow_clim", "fcst_type"]).sort_index()
+
+    return det_verif, prob_verif
+
 
 # %% DIFFERENT DMB APPROACHES:
+site = "Tumlingtar";  date_range = ['20140101', '20141231']
+obs_dir, fcst_data = get_fcst_data ( date_range, site)
+
 
 # bias correction approaches:
 fcst_types = ["Q_raw", "Q_dmb", "Q_ldmb", "Q_dmb-var", "Q_ldmb-var"]
@@ -46,14 +159,15 @@ days  = range(1,11)
 # window length:
 win_len = 3
 
+#%%
 det_verif, prob_verif = dmb_vars_test(fcst_types, days, win_len, site, fcst_data, obs_dir)
 # %% 
 fig, fig_crps = kge_crps_plttr (det_verif, prob_verif, site, fcst_types)
 
 
 # %% Plot KGE horizon:
-
-fig.show(renderer = 'iframe')
+fig.show()
 
 # %% Plot CRPS 
-fig_crps.show(renderer = 'iframe')
+fig_crps.show()
+# %%
--- 3_Scripts/5_bias_crrction_calibration.py
+++ 3_Scripts/5_bias_crrction_calibration.py
@@ -1,4 +1,4 @@
-from datetime import date
+# %%
 import pandas as pd
 import warnings
 # import all functions:
@@ -7,6 +7,7 @@ from plt_funcs import *
 
 warnings.filterwarnings("ignore")
 
+# %%
 """
 ####### Initialization parameters and get frcst data ######
 """
@@ -35,6 +36,8 @@ def get_fcst_data (date_range, site):
 
     return obs_dir, fcst_data
 
+
+
 # %% ######################################### %% #
 ############# Load Forecast Data ################
 site = "Tumlingtar";  date_range = ['20140101', '20141231']
@@ -44,9 +47,10 @@ obs_dir, fcst_data = get_fcst_data ( date_range, site)
 ##### Calibration for multiple horizons ###########
 # calibrator runs across multiple window lengths for dmb correction:
 
-# fcst_types = ["Q_raw", "Q_dmb", "Q_ldmb", "Q_dmb-var", "Q_ldmb-var"]
-days = [4, 7, 9]
-# days = range(1,11)
+fcst_types = ["Q_raw", "Q_dmb", "Q_ldmb", "Q_dmb-var", "Q_ldmb-var"]
+days = [11]
+# days = range(1,16)
+# days = [1,3,8,13]
 
 lo_verif    = []
 hi_verif    = []
@@ -56,7 +60,56 @@ for day in days:
     [fcst_data_day, clim_vals] = add_obs(
     place = site, fcst_df = fcst_data, obs_dir = obs_dir, day = day)
     # optimum calibration:
-    lo_df, hi_df, prob_df = fcst_calibrator (fcst_data_day, clim_vals)
+    # lo_df, hi_df, prob_df = fcst_calibrator (fcst_data_day, clim_vals, fcst_types)
+
+    # windows     = [2,5]
+
+    windows     = [2, 3, 5, 7, 10, 15, 20, 30]
+    lo_df    = []
+    hi_df    = []
+    prob_df  = []
+    for win_len in windows:
+        # l_df, h_df, bc_df, p_df, det_df =  post_process(fcst_data_day, win_len, 
+        #                     clim_vals, fcst_types)
+
+        fcst_types = ["Q_raw", "Q_dmb", "Q_ldmb", "Q_dmb-var", "Q_ldmb-var"]
+        if fcst_types == ["Q_raw", "Q_dmb", "Q_ldmb", "Q_dmb-var", "Q_ldmb-var"]:
+            bc_df = bc_fcsts_variations(fcst_data_day, win_len)
+        else :
+            # Bias correct the forecasts using DMB and LDMB
+            bc_df = bc_fcsts(df = fcst_data_day, win_len = win_len )
+
+        fcst_types = bc_df.columns[bc_df.columns.str.startswith("Q")].values.tolist()
+        # Separate dataframes for deterministic forecasts:
+        # df = t1.reset_index()
+        df_det = det_frcsts(bc_df.reset_index(), fcst_types)
+
+        # calculate the metrics:
+        l_df, h_df = metric_calc(df_det, bc_df, clim_vals, fcst_types)
+
+        print(fcst_types)
+
+        # calculate probabilitic verification (CRPS):
+        p_df = prob_metrics(bc_df, clim_vals, fcst_types)        
+
+        l_df["win_length"]     = win_len
+        h_df["win_length"]     = win_len
+        p_df["win_length"]   = win_len
+        lo_df.append(l_df) 
+        hi_df.append(h_df)
+        prob_df.append(p_df)
+
+    # create a single large dataframe for low and high flow seasons:
+    lo_df    = pd.concat(lo_df)
+    hi_df    = pd.concat(hi_df)
+    prob_df  = pd.concat(prob_df)
+
+    lo_df = lo_df.set_index(["win_length", "fcst_type"], append= True
+                    ).reorder_levels(["win_length", "fcst_type", "det_frcst"]).sort_index()
+    hi_df = hi_df.set_index(["win_length", "fcst_type"], append= True
+                    ).reorder_levels(["win_length", "fcst_type", "det_frcst"]).sort_index()
+    prob_df = prob_df.set_index(["win_length"], append= True
+                    ).reorder_levels(["win_length", "flow_clim", "fcst_type"]).sort_index()
 
     lo_df["day"]   = day
     hi_df["day"]   = day
@@ -71,11 +124,11 @@ for day in days:
 
     for flo_con in flo_conditions:
         fig = calibrtn_plttr (hi_df, lo_df, prob_df, site, day, 
-                flo_con)     
+                flo_con, fcst_types=["Q_dmb_2", "Q_ldmb"])     
     
         # save the figure
-        save_pth = f'./iframe_figures/{site}-calibration-day_{day}-flow_{flo_con}.html' 
-        # fig.show()
+        save_pth = f'../4_results/08-calibration/{site}-calibration-day_{day}-flow_{flo_con}.html' 
+        fig.show()
         fig.write_html(save_pth)
 
 # create a single large dataframe for low and high flow seasons:
@@ -96,7 +149,7 @@ prob_verif  = prob_verif.set_index(["day"], append= True
                     "fcst_type"]).sort_index()
 
 # %%
-# use only the ensemble median, drop the raw forecasts as it does 
+# use only the ensemble median, drop the raw forecasts as it does not
 # determine which calibration window is best. 
 # level = [1,2] = [day, flow_clim]
 
@@ -114,3 +167,9 @@ prob_sub = prob_verif.drop('Q_raw', level = 'fcst_type').reset_index(level = [1,
 # final metric:
 optim_cal_window = det_sub.join(prob_sub)
 optim_cal_window
+
+#%%
+save_dir         = f"../4_results/08-calibration/{site}_opt_cal.csv"
+optim_cal_window.to_csv(save_dir)
+
+            # %%
